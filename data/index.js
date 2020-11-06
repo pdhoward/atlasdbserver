d8ca2fdf6053c504b971eba8ab0a8933a04b69b8
pdhoward <patrick.howard@hotmail.com>
2020-11-06 10:22:34 -0500

    initialize

---
+++ data/index.js
@@ -0,0 +1,256 @@
+
+exports.catalogSeed = [
+    {
+      productname: "library",
+      description: "Day At The Library",
+      image: "/assets/images/1.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "forests",
+      description: "Forests Overhead",
+      image: "/assets/images/2.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "brick",
+      description: "Brick Wall",
+      image: "/assets/images/3.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "door",
+      description: "Abstract Door In The Wall",
+      image: "/assets/images/4.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "ring",
+      description: "Focused Ring",
+      image: "/assets/images/5.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "taxi",
+      description: "Taxi Cab",
+      image: "/assets/images/6.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "building",
+      description: "A Tall Building",
+      image: "/assets/images/7.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "pathway",
+      description: "Ethereal Pathway",
+      image: "/assets/images/8.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "ribbon",
+      description: "Cutting The Ribbon On Opening Day",
+      image: "/assets/images/9.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "unusual",
+      description: "New experiences for another day",
+      image: "/assets/images/10.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "bridge",
+      description: "There is a man On Bridge",
+      image: "/assets/images/11.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "book",
+      description: "Floating Book in the lake of fire",
+      image: "/assets/images/12.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "searchall",
+      description: "Searching for the next opportunity",
+      image: "/assets/images/13.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "wall",
+      description: "Flyers On The Wall",
+      image: "/assets/images/14.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "floor",
+      description: "Caution, Wet Floor",
+      image: "/assets/images/15.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "sunset",
+      description: "Sunset over the Pacific Northwest",
+      image: "/assets/images/16.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "museum",
+      description: "Museum of Innovation and change",
+      image: "/assets/images/17.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "morning",
+      description: "Foggy Morning on the banks of the river",
+      image: "/assets/images/18.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "lookup",
+      description: "Look Up and see the glory of God",
+      image: "/assets/images/19.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "city",
+      description: "City on the Hill ",
+      image: "/assets/images/20.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "garage",
+      description: "Parking Garage in the Urabn area",
+      image: "/assets/images/21.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "giraffe",
+      description: "Giraffe walking down the street",
+      image: "/assets/images/22.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "rocks",
+      description: "On The Rocks of the Coast of Ireland",
+      image: "/assets/images/23.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "airline",
+      description: "Airline Puddle and cancellations ",
+      image: "/assets/images/24.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "good",
+      description: "Good Boy doing your math. How about english",
+      image: "/assets/images/25.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "groove",
+      description: "Grooves in the counter top and the ceiling",
+      image: "/assets/images/26.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "brick",
+      description: "Brick Photo of a patio and wall",
+      image: "/assets/images/27.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "blue",
+      description: "Blue sky heading my way",
+      image: "/assets/images/28.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "rings",
+      description: "Count The Rings on the fingers and shout",
+      image: "/assets/images/29.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "sew",
+      description: "Sewing patch on this software",
+      image: "/assets/images/30.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "ice cream",
+      description: "Ice Cream maker on the farm porch",
+      image: "/assets/images/31.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "cactus",
+      description: "Cactus in the living room and the desert",
+      image: "/assets/images/32.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "hamster",
+      description: "Hampster running uphill and in the wheel cage",
+      image: "/assets/images/33.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "technology",
+      description: "Technology cannot help everyone only someone",
+      image: "/assets/images/34.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "church",
+      description: "Notre Dame is a church in Paris and france",
+      image: "/assets/images/35.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    },
+    {
+      productname: "railroad",
+      description: "Railroad tracks laid out for miles in front of us",
+      image: "/assets/images/36.jpg",
+      rating: 0,
+      date: new Date(Date.now())
+    }
+  ];
+  
